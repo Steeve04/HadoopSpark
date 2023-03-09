6cb9ad4067a2ec72e00b57a0ed501b7857833e92
Steeve04 <56684419+Steeve04@users.noreply.github.com>
2023-03-09 17:02:21 +0100

    Update avg_dist_satisfaction.py

--- avg_dist_satisfaction.py
+++ avg_dist_satisfaction.py
@@ -1,4 +1,6 @@
 import csv
+import pandas as pd
+from pymongo import MongoClient
 
 # Fonction Mapper
 def mapper(row):
@@ -38,3 +40,36 @@ with open("test.csv", "r") as f:
     for key, values in results.items():
         average_distance = reducer(key, values)
         average_distance_l.append(average_distance)
+
+    # Stockage sous format liste de dictionnaire
+    avg_dist_dict = [{item[0]: str(item[1])} for item in average_distance_l]
+
+    # Connexion à la base de MongoDB avec user='test', password='pwd', cluster='cluster.1t7qkeq'
+    client = MongoClient("mongodb+srv://test:pwd@cluster.1t7qkeq.mongodb.net/?retryWrites=true&w=majority")
+
+    # Affectation de db à la base
+    db = client['Satisfaction']
+
+    # Affectation à la collection
+    avg_dist_collection = db['Distance']
+
+    # Insertion de la liste de dictionnaire dans la collection de la base de donnée MongoDB
+    avg_dist_collection.insert_many(avg_dist_dict)
+
+    # Récupération des données sous forme de liste de dictionnaires
+    data_coll = list(avg_dist_collection.find())
+
+    # Transformation des données en DataFrame
+    df_avg_dist = pd.DataFrame(data_coll)
+
+    # Pivotage des colonnes satisfied et neutral or dissatisfied en Satisfaction
+    df_avg_dist = df_avg_dist.melt(id_vars=['_id'], value_vars=['satisfied', 'neutral or dissatisfied'], var_name='Satisfaction', value_name='Distance_moyenne')
+    
+    # Garde seulement les colonnes nécessaires
+    df_avg_dist = df_avg_dist[['Satisfaction', 'Distance_moyenne']]
+    
+    # Suppression des valeurs NaN
+    df_avg_dist = df_avg_dist.dropna()
+    
+    # Déconnexion à la base
+    client.close()
